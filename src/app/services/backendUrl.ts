c0d9649ecb586be7ec03068a4b8b14d79fbb5bbd
Farhat Sharif <farhatsharif.it@gmail.com>
2020-11-17 16:30:05 +0500

    Update backend API URL

--- src/app/services/backendUrl.ts
+++ src/app/services/backendUrl.ts
@@ -1,2 +1,1 @@
-export const BACKEND_URL = 'http://188.166.37.56:3001/api/';
-// export const BACKEND_URL = 'http://localhost:3001/api/';
+export const BACKEND_URL = 'http://www.mini-blog-api.projects.farhatsharif.com/api/';
